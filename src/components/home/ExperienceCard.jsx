a3f9f17c25ca5892d069399c1ec7a106b033a91a
anmolverma404 <anmolverma102002@gmail.com>
2024-10-17 16:31:11 +0530

    Add experience url if preesnt

--- src/components/home/ExperienceCard.jsx
+++ src/components/home/ExperienceCard.jsx
@@ -1,23 +1,38 @@
 import React from 'react';
 
-import {
-  Col,
-} from "react-bootstrap";
+import { Col } from 'react-bootstrap';
 
 const ExperienceCard = ({ data }) => {
-  return (
-    <Col lg="6">
-      <div className="pb-5 text-center">
-        <img className=" bg-white mb-3 experience__row__img" src={data.companylogo} alt="" width="500px" height="auto" />
-        <p className="lead">
-          {data.role}
-          <br />
-          {data.date}
-        </p>
-
-      </div>
-    </Col>
-  );
-}
+	return (
+		<Col lg="6">
+			<div className="pb-5 text-center">
+				{data.siteUrl ? (
+					<a href={data.siteUrl} target="_blank" rel="noreferrer">
+						<img
+							className="bg-white mb-3 experience__row__img"
+							src={data.companylogo}
+							alt=""
+							width="500px"
+							height="auto"
+						/>
+					</a>
+				) : (
+					<img
+						className="bg-white mb-3 experience__row__img"
+						src={data.companylogo}
+						alt=""
+						width="500px"
+						height="auto"
+					/>
+				)}
+				<p className="lead">
+					{data.role}
+					<br />
+					{data.date}
+				</p>
+			</div>
+		</Col>
+	);
+};
 
 export default ExperienceCard;
--- src/editable-stuff/config.js
+++ src/editable-stuff/config.js
@@ -1,29 +1,29 @@
 // Navigation Bar SECTION
 const navBar = {
-  show: true,
+	show: true,
 };
 
 // Main Body SECTION
 const mainBody = {
-  gradientColors: "#4484ce, #1ad7c0, #ff9b11, #9b59b6, #ff7f7f, #ecf0f1",
-  firstName: "Anmol",
-  middleName: "",
-  lastName: "Verma",
-  message: " I Code :) Quotes not found 404 ;) ",
-  icons: [
-    {
-      image: "fa-github",
-      url: "https://github.com/AnmolVerma404",
-    },
-    {
-      image: "fa-linkedin",
-      url: "https://www.linkedin.com/in/anmolverma404/",
-    },
-    {
-      image: "fa-twitter",
-      url: "https://twitter.com/anmolverma404",
-    },
-  ],
+	gradientColors: '#4484ce, #1ad7c0, #ff9b11, #9b59b6, #ff7f7f, #ecf0f1',
+	firstName: 'Anmol',
+	middleName: '',
+	lastName: 'Verma',
+	message: ' I Code :) Quotes not found 404 ;) ',
+	icons: [
+		{
+			image: 'fa-github',
+			url: 'https://github.com/AnmolVerma404',
+		},
+		{
+			image: 'fa-linkedin',
+			url: 'https://www.linkedin.com/in/anmolverma404/',
+		},
+		{
+			image: 'fa-twitter',
+			url: 'https://twitter.com/anmolverma404',
+		},
+	],
 };
 
 // ABOUT SECTION
@@ -37,15 +37,15 @@ const mainBody = {
 //d) If you do not want any picture to be displayed, just leave it empty :)
 //      i.e: profilePictureLink: "",
 
-
 const about = {
-  show: true,
-  heading: "About Me",
-  imageLink: require("../editable-stuff/anmolverma.jpeg"),
-  imageSize: 375,
-  message:
-    "Hey there! I'm Anmol Verma, a graduate from Vellore Institute of Technology Bhopal, where I spent 2020-2024 learning the art of turning coffee into code. I majored in Computer Engineering, which is a fancy way of saying I spend a lot of time convincing computers to do what I want (and occasionally begging them to cooperate). Currently, I'm rocking the software engineering scene at rtCamp. When I'm not buried under lines of code, you can find me tinkering with projects, dreaming up the next big thing, or plotting ways to convince my computer to clean up its own bugs.",
-  resume: "https://drive.google.com/file/d/1HcHY8d4PNzcmr0E2IwjAhSQqJxHSsyk-/view",
+	show: true,
+	heading: 'About Me',
+	imageLink: require('../editable-stuff/anmolverma.jpeg'),
+	imageSize: 375,
+	message:
+		"Hey there! I'm Anmol Verma, a graduate from Vellore Institute of Technology Bhopal, where I spent 2020-2024 learning the art of turning coffee into code. I majored in Computer Engineering, which is a fancy way of saying I spend a lot of time convincing computers to do what I want (and occasionally begging them to cooperate). Currently, I'm rocking the software engineering scene at rtCamp. When I'm not buried under lines of code, you can find me tinkering with projects, dreaming up the next big thing, or plotting ways to convince my computer to clean up its own bugs.",
+	resume:
+		'https://drive.google.com/file/d/1HcHY8d4PNzcmr0E2IwjAhSQqJxHSsyk-/view',
 };
 
 // PROJECTS SECTION
@@ -54,78 +54,93 @@ const about = {
 // If you want to display specfic projects, add the repository names,
 //      i.e ["repository-1", "repo-2"]
 const repos = {
-  show: true,
-  heading: "Pinned Repository",
-  gitHubUsername: "AnmolVerma404",
-  reposLength: 0,
-  specificRepos: ["TicketOps","Healthy-HearTechs","MapMyPath","Data-Structures-and-Algorithms"],
+	show: true,
+	heading: 'Pinned Repository',
+	gitHubUsername: 'AnmolVerma404',
+	reposLength: 0,
+	specificRepos: [
+		'TicketOps',
+		'Healthy-HearTechs',
+		'MapMyPath',
+		'Data-Structures-and-Algorithms',
+	],
 };
 
 // Leadership SECTION
 const leadership = {
-  show: false,
-  heading: "Leadership",
-  message:
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vitae auctor eu augue ut lectus arcu bibendum at varius. Libero justo laoreet sit amet cursus sit amet. Imperdiet dui accumsan sit amet nulla facilisi morbi. At auctor urna nunc id. Iaculis urna id volutpat lacus laoreet non curabitur gravida. Et magnis dis parturient montes nascetur ridiculus mus mauris. In nisl nisi scelerisque eu ultrices vitae auctor. Mattis nunc sed blandit libero volutpat sed cras ornare. Pulvinar neque laoreet suspendisse interdum consectetur libero.",
-  images: [
-    { 
-      img: require("../editable-stuff/anmolverma.jpeg"), 
-      label: "First slide label", 
-      paragraph: "Nulla vitae elit libero, a pharetra augue mollis interdum." 
-    },
-    { 
-      img: require("../editable-stuff/anmolverma.jpeg"), 
-      label: "Second slide label", 
-      paragraph: "Nulla vitae elit libero, a pharetra augue mollis interdum." 
-    },
-  ],
-  imageSize: {
-    width:"615",
-    height:"450"
-  }
+	show: false,
+	heading: 'Leadership',
+	message:
+		'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vitae auctor eu augue ut lectus arcu bibendum at varius. Libero justo laoreet sit amet cursus sit amet. Imperdiet dui accumsan sit amet nulla facilisi morbi. At auctor urna nunc id. Iaculis urna id volutpat lacus laoreet non curabitur gravida. Et magnis dis parturient montes nascetur ridiculus mus mauris. In nisl nisi scelerisque eu ultrices vitae auctor. Mattis nunc sed blandit libero volutpat sed cras ornare. Pulvinar neque laoreet suspendisse interdum consectetur libero.',
+	images: [
+		{
+			img: require('../editable-stuff/anmolverma.jpeg'),
+			label: 'First slide label',
+			paragraph: 'Nulla vitae elit libero, a pharetra augue mollis interdum.',
+		},
+		{
+			img: require('../editable-stuff/anmolverma.jpeg'),
+			label: 'Second slide label',
+			paragraph: 'Nulla vitae elit libero, a pharetra augue mollis interdum.',
+		},
+	],
+	imageSize: {
+		width: '615',
+		height: '450',
+	},
 };
 
 // SKILLS SECTION
 const skills = {
-  show: true,
-  heading: "Skills",
-  showHardSkills:true,
-  hardSkills: [
-    { name: "C++", value: 90 },
-    { name: "Data Structures", value: 85 },
-    { name: "JavaScript", value: 90 },
-    { name: "TypeScript", value: 70 },
-    { name: "React", value: 65 },
-    { name: "HTML/CSS", value: 80 },
-    { name: "PHP", value: 80 },
-    { name: "DevOps", value: 50 },
-  ],
+	show: true,
+	heading: 'Skills',
+	showHardSkills: true,
+	hardSkills: [
+		{ name: 'C++', value: 90 },
+		{ name: 'Data Structures', value: 85 },
+		{ name: 'JavaScript', value: 90 },
+		{ name: 'TypeScript', value: 70 },
+		{ name: 'React', value: 65 },
+		{ name: 'HTML/CSS', value: 80 },
+		{ name: 'PHP', value: 80 },
+		{ name: 'DevOps', value: 50 },
+	],
 };
 
 // GET IN TOUCH SECTION
 const getInTouch = {
-  show: true,
-  heading: "Get In Touch",
-  message:
-    "Thank you for visiting 🙌 If you have any questions or inquiries, feel free to reach out via email 📩",
-  email: "anmolverma102002@gmail.com",
+	show: true,
+	heading: 'Get In Touch',
+	message:
+		'Thank you for visiting 🙌 If you have any questions or inquiries, feel free to reach out via email 📩',
+	email: 'anmolverma102002@gmail.com',
 };
 
 const experiences = {
-  show: true,
-  heading: "Experiences",
-  data: [
-    {
-      role: 'Software Engineer',
-      companylogo: require('../assets/img/rtCamp.png'),
-      date: 'Oct 2023 – Present',
-    },
-  ]
-}
+	show: true,
+	heading: 'Experiences',
+	data: [
+		{
+			role: 'Software Engineer',
+			companylogo: require('../assets/img/rtCamp.png'),
+			date: 'Oct 2023 – Present',
+			siteUrl: 'https://rtcamp.com/',
+		},
+	],
+};
 
 // Blog SECTION
 // const blog = {
 //   show: true,
 // };
 
-export { navBar, mainBody, about, repos, skills, leadership, getInTouch, experiences };
+export {
+	navBar,
+	mainBody,
+	about,
+	repos,
+	skills,
+	leadership,
+	getInTouch,
+	experiences,
+};
