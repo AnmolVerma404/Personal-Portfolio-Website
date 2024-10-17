eb5c1d4108a6f63d207b1dac268e894046a62db9
anmolverma404 <anmolverma102002@gmail.com>
2024-10-17 16:32:19 +0530

    Follow DRY on Experience card component

--- src/components/home/ExperienceCard.jsx
+++ src/components/home/ExperienceCard.jsx
@@ -3,28 +3,19 @@ import React from 'react';
 import { Col } from 'react-bootstrap';
 
 const ExperienceCard = ({ data }) => {
+	const ImgElement = (
+		<img
+			className="bg-white mb-3 experience__row__img"
+			src={data.companylogo}
+			alt=""
+			width="500px"
+			height="auto"
+		/>
+	);
 	return (
 		<Col lg="6">
 			<div className="pb-5 text-center">
-				{data.siteUrl ? (
-					<a href={data.siteUrl} target="_blank" rel="noreferrer">
-						<img
-							className="bg-white mb-3 experience__row__img"
-							src={data.companylogo}
-							alt=""
-							width="500px"
-							height="auto"
-						/>
-					</a>
-				) : (
-					<img
-						className="bg-white mb-3 experience__row__img"
-						src={data.companylogo}
-						alt=""
-						width="500px"
-						height="auto"
-					/>
-				)}
+				{data.siteUrl ? <a href={data.siteUrl}>{ImgElement}</a> : ImgElement}
 				<p className="lead">
 					{data.role}
 					<br />
